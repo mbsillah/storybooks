1bd97a38dc17c4b4a3a0fb34e24a7649b254909f
sillah2010 <macktastic2014@gmail.com>
2020-06-25 19:38:08 -0400

    Added google authentication

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 //Load config
 dotenv.config({ path: './config/config.env' });
+
+//Passport config
+require('./config/passport')(passport);
 
 //Connect to DB
 connectDB();
@@ -23,11 +28,25 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+//Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Static Folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 //Login Page/Landing Page
 //route GET
 
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
   res.render('login', {
     layout: 'login',
   });
@@ -13,7 +14,7 @@ router.get('/', (req, res) => {
 //Dashboard
 //route GET
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
   res.render('dashboard');
 });
 
