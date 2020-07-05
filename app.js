ffe5754d6afdbb2aa00b1d0d047f608f7f0b9177
sillah2010 <macktastic2014@gmail.com>
2020-07-05 15:34:04 -0400

    Latest push or stories

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const connectDB = require('./config/db');
 
 //Load config
@@ -19,13 +21,27 @@ connectDB();
 //Start App
 const app = express();
 
+//Body parser
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.json());
+
 //Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//HandleBars helpers
+const { formatDate } = require('./helpers/hbs');
+
 //Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine(
+  '.hbs',
+  exphbs({ helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs' })
+);
 app.set('view engine', '.hbs');
 
 //Sessions
@@ -34,6 +50,7 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
@@ -47,6 +64,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 5000;
 
